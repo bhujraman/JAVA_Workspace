15c3c84c8bdc87b2e8e3820a3496da897a734125
bhujraman <130060094+bhujraman@users.noreply.github.com>
2023-04-10 11:13:51 +0530

    Add files via upload

---
+++ src/com/extra/VolatileEg.java
@@ -0,0 +1,37 @@
+package com.extra;
+
+
+public class VolatileEg {
+     public static int flag = 0;
+	public static void main(String[] args) {
+		new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				while(true){
+					if(flag == 0){
+						System.out.println("Running");
+					}else{
+						break;
+					}
+				}	
+			}
+		}).start();
+		
+        new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					
+					e.printStackTrace();
+				}
+				VolatileEg.flag = 1; 
+				System.out.println("Flag value updated");
+			}
+		}).start();
+	}
+
+}
